01fe3c5f265e52726ef6d4d9dea289b0f70dd7aa
Kris Szafranski <kdszafranski@gmail.com>
2016-10-30 15:48:44 -0500

    clarified instructions

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -3,4 +3,5 @@
 
 	// 2 - Change the text of the h1 element to something (anything!) else.
 
-  // 3 - Get rid of (delete) the useless unordered list by first selecting the h2 and traversing upward.
+  // 3 - Get rid of (delete) the useless unordered list (ul) by first
+  // selecting the h2 nested within its list item.
