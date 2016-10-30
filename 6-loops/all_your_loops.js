04f8870266473c1cc16d400498092bb9d60deea1
Kris Szafranski <kdszafranski@gmail.com>
2016-10-30 15:27:58 -0500

    updated loop challenges

--- 6-loops/all_your_loops.js
+++ 6-loops/all_your_loops.js
@@ -7,3 +7,13 @@ var checkz = 0;
 // 2 - Write a while loop that runs
 // 3 times. Each iteration will decrease
 // the value of checkz by -2
+
+// 3 - Summarize in English what this code is doing.
+// Use a multiline comment to do so.
+//
+var numbers = [1, 2, 3, 4, 5, 6, 7];
+var total = 0;
+for (var i = 0; i < numbers.length; i++) {
+  total += numbers[i];
+}
+console.log(total);
--- 8-arrays/advanced.js
+++ 8-arrays/advanced.js
@@ -1,4 +1,5 @@
-// 1 - Use a built-in method of Arrays to add the value 32 to the end of
+// 1 - Use a built-in method of Arrays
+// to add the value 32 to the end of
 // the following array.
 
 var arr = [3, 4, 6, 0];
