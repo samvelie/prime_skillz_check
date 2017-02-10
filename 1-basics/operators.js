de098ff3d32d5c7127508e8d180978e90e5a9c1d
Kris Szafranski <kdszafranski@gmail.com>
2017-02-10 14:13:46 -0600

    updated for phi

--- 1-basics/operators.js
+++ 1-basics/operators.js
@@ -1,7 +1,7 @@
-// 1 - Assign the value 22 to a variable called sigma
+// 1 - Assign the value 22 to a variable called phi
 
-// 2 - Create a new variable called peeps and assign it the value of sigma
+// 2 - Create a new variable called peeps and assign it the value of phi
 
 // 3 - Decrease the value of peeps by 1
 
-// 4 - Write a console log that checks if sigma is equal to peeps
+// 4 - Write a console log that checks if phi is equal to peeps
