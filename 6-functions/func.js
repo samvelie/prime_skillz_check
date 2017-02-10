0d9aad03cc40ecc70278badd7557b720006f03fc
Kris Szafranski <kdszafranski@gmail.com>
2017-02-10 14:38:01 -0600

    updated functions

--- 6-functions/func.js
+++ 6-functions/func.js
@@ -11,12 +11,3 @@
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
-
-
-// 4 - Write a function that calls the
-// function from #2 above. The first argument
-// should be provided by calling the
-// function from #1 and the second argument
-// by passing in a number of your choice.
-// This new function should console log the
-// results of calling function #2.
