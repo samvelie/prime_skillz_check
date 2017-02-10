19abb0da1904708c25b5865d45e5c04d4b944949
Kris Szafranski <kdszafranski@gmail.com>
2017-02-10 14:20:47 -0600

    marked switch for skipping

--- 3-conditionals/switch.js
+++ 3-conditionals/switch.js
@@ -1,4 +1,7 @@
-// Write a switch statement that 
+// PHI DO NOT DO
+
+
+// Write a switch statement that
 // console logs witty statements for each
 // day of the standard work week: Monday, Tuesday,
 // Wednesday, Thursday, Friday
